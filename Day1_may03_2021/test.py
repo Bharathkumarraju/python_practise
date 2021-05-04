c1ef0f0b675b6faa648a592aa0f5861ca532a2d0
Bharath <bharath@hooq.tv>
2021-05-04 17:54:14 +0800

    Add the tests

---
+++ Day1_may03_2021/test.py
@@ -0,0 +1,66 @@
+
+# b = list('a','b','c')
+
+# print(a[1])
+
+# print(type(a))
+# print(len(a))
+# # for idx,items in enumerate(a):
+# # 	print("{}, \t {},\t\t {}".format(idx, items, type(items)))
+# a[2] = 6
+# print(a)
+
+# # Ordered
+# # Changeble
+# # Allow duplicates
+
+# # %d -- integer
+# # %f -- float
+# # %s -- string
+
+# int a = 5;
+
+## class is called user defined data types
+
+## constructor
+
+class test_a:
+    def __init__():
+        a = 5
+        b = 10
+
+
+class test:
+    ##constructor for class
+
+    ## to initialize a class
+    ## to create an object of that class
+    def __init__(self ,i ,j):
+        self.a = 5
+        self.b = 6
+        a = 11
+    # main()
+    # print(self.a)
+
+    # every function we define inside class, we need to pass an argument "self" which points to the object of that class
+    def main(self):
+        print(self.a)
+
+
+# dynamic memory allocation
+
+b = list(('a' ,'b' ,'c'))
+print(b)
+integer_1 = int(5)
+print(integer_1)
+
+# test_variable = test(5,6)
+
+
+# test_variable.main()
+
+# # print(test_variable.a)
+
+# # custom_variable = test
+# # print(custom_variable.a[1])
+

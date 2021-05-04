8a74a110dbd39986bfbc5d94cd473714e1b5e045
Bharath <bharath@hooq.tv>
2021-05-04 21:47:26 +0800

    Python class usage is to code reusability

---
+++ Day2_may04_2021/practices/output1.sh
@@ -0,0 +1,7 @@
+/usr/local/bin/python3.7 /Users/bharathdasaraju/git/python_practise/Day2_may04_2021/practices/demo1.py
+['maths', 'science', 'history']
+['engineering', 'medicine']
+['music', 'sports', 'psychology']
+
+Process finished with exit code 0
+

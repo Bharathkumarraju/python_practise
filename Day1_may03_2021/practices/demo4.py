be1f36974376a9569b80c0d951734b69709f2e6b
Bharath <bharath@hooq.tv>
2021-05-04 19:19:20 +0800

    All about python classes even list dict int str float all data types are classes only

---
+++ Day1_may03_2021/practices/demo4.py
@@ -0,0 +1,10 @@
+# Dynamic memory allocation
+a = list(("1", "2", "3"))  # Even list is also a class in python,  here we are creating object "a" of class list.
+print(a)
+print(type(a)) # It prints class list
+
+print("-----------------------------------------")
+
+b = int(18) # Even int is also a class in python, Here we are creating object "b" of class int.
+print(b)
+print(type(b)) # It prints class int
---
+++ Day1_may03_2021/practices/output4.sh
@@ -0,0 +1,10 @@
+/usr/local/bin/python3.7 /Users/bharathdasaraju/git/python_practise/Day1_may03_2021/practices/demo4.py
+['1', '2', '3']
+<class 'list'>
+-----------------------------------------
+18
+<class 'int'>
+
+Process finished with exit code 0
+
+

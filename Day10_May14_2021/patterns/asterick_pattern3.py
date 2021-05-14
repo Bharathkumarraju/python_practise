43ee846fc3553da1bb3e309842ad7d69d72ab3e0
Bharathkumar Dasaraju <bharathkumar.dasaraju@singtel.com>
2021-05-14 12:09:07 +0800

    patterns with asterick pyramid

---
+++ Day10_May14_2021/patterns/asterick_pattern3.py
@@ -0,0 +1,17 @@
+"""
+    *
+   * *
+  * * *
+ * * * *
+* * * * *
+
+"""
+num = int(input("Enter a Number"))
+
+for i in range(0, num):
+    for j in range(0, num-i-1):
+        print(" ", end="")
+    for j in range(0, i + 1):
+        print("*", end=" ")
+    print()
+

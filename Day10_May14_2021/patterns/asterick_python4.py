66edc1d52f1365249ccd48ede80fe361d87129f0
Bharathkumar Dasaraju <bharathkumar.dasaraju@singtel.com>
2021-05-14 15:35:16 +0800

    pyramids in reverse order

---
+++ Day10_May14_2021/patterns/asterick_python4.py
@@ -0,0 +1,23 @@
+"""
+*******
+ *****
+  ***
+   *
+"""
+for i in range(1,11,2):
+    print(i)
+
+print("-----------------------------")
+
+for i in range(5, 0, -1):
+    print(i)
+
+print("-----------------------------")
+
+num = int(input("Enter a value"))
+for i in range(num, 0, -1):
+    for j in range(0, num-i):
+        print(" ", end="")
+    for j in range(0, i):
+        print("* ", end="")
+    print()

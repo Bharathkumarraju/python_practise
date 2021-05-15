da9465e43b50b6b19fb8acd1300273654b760c67
Bharath <bharath@hooq.tv>
2021-05-15 10:25:07 +0800

    inverted right angel trinagle

---
+++ Day10_May14_2021/patterns/asterick_pattern_6_inverted_right_angle_triangle_2.py
@@ -0,0 +1,15 @@
+"""
+* * * * *
+* * * *
+* * *
+* *
+*
+
+"""
+
+num=int(input("Enter the number of rows: "))
+
+for i in range(num, 0, -1):
+    for j in range(0,i):
+        print("*", end="")
+    print("")

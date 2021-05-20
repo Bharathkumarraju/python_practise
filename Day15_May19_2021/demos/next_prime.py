0e9c5f6bec716ee4e878232de0c32e955cfa4f14
Bharath <bharath@hooq.tv>
2021-05-20 08:59:16 +0800

    find next prime number

---
+++ Day15_May19_2021/demos/next_prime.py
@@ -0,0 +1,19 @@
+def main():
+    n = int(input('Find the next prime number greater great than: '))
+    prime=find_next_prime(n+1)
+    print("next prime is: ", prime)
+
+def find_next_prime(n):
+    return find_prime_in_range(n, 2*n)
+
+def find_prime_in_range(a, b):
+    for p in range(a, b):
+        for i in range(2, p):
+            if p % i == 0:
+                break
+        else:
+            return p
+    return None
+
+if __name__ == '__main__':
+    main()

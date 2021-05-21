f77dd52ab43e6df8b8c628c94f78f8df4f665239
Bharath <bharath@hooq.tv>
2021-05-21 08:32:29 +0800

    everything in python is an object and can see with type() and also how to see all methods using dir()

---
+++ Day17_May21_2021/Everything_is_an_object/proof1.py
@@ -0,0 +1,23 @@
+numbers = [1, 4, 9, 10]
+print(type(numbers))
+
+
+n1 = 5
+print(type(n1))
+
+flag = True
+print(type(flag))
+
+def my_function():
+    pass
+
+print(type(my_function))
+
+# output shows classes that means all above are objects instantiated from the classes.
+
+"""
+<class 'list'>
+<class 'int'>
+<class 'bool'>
+<class 'function'>
+"""

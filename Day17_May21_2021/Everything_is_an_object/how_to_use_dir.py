6b18ee872130f1bdb9ebf9cf00399b13c75f0986
Bharath <bharath@hooq.tv>
2021-05-21 09:09:41 +0800

    All about python dir()

--- Day17_May21_2021/Everything_is_an_object/how_to_use_dir.py
+++ Day17_May21_2021/Everything_is_an_object/how_to_use_dir.py
@@ -2,15 +2,17 @@
 We can list out all the attributes and methods of a given object by using the dir() function.
 """
 
+"""
+['__add__', '__class__', '__contains__', '__delattr__', '__delitem__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__gt__', '__hash__', '__iadd__', '__imul__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__reversed__', '__rmul__', '__setattr__', '__setitem__', '__sizeof__', '__str__', '__subclasshook__', 'append', 'clear', 'copy', 'count', 'extend', 'index', 'insert', 'pop', 'remove', 'reverse', 'sort']
+[1, 2, 3, 4, 5, 6]
+"""
+
+
 numbers_list = [1, 2, 3]
 print(dir(numbers_list))
 
 result = numbers_list.__add__([4, 5, 6])
 print(result)
 
-
-
-"""
-['__add__', '__class__', '__contains__', '__delattr__', '__delitem__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__gt__', '__hash__', '__iadd__', '__imul__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__reversed__', '__rmul__', '__setattr__', '__setitem__', '__sizeof__', '__str__', '__subclasshook__', 'append', 'clear', 'copy', 'count', 'extend', 'index', 'insert', 'pop', 'remove', 'reverse', 'sort']
-[1, 2, 3, 4, 5, 6]
-"""
+result1 = result + [7, 8, 9]
+print(result1)

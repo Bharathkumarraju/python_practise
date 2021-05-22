2f468378e6c6beb7a3ffbc943748e63616fc16c8
Bharath <bharath@hooq.tv>
2021-05-22 11:07:14 +0800

    All about os modules

--- Day18_May22_2021/os_modules/get_current_dir.py
+++ Day18_May22_2021/os_modules/get_current_dir.py
@@ -2,3 +2,10 @@ import os
 
 current_dir = os.getcwd()
 print(current_dir)
+
+os.chdir("/Users/bharathdasaraju/git/python_practise/Day18_May22_2021/demos")
+print(os.getcwd())
+
+with open("test.txt", 'w') as f:
+    f.write("This is a test file")
+
---
+++ Day18_May22_2021/os_modules/make_dirs.py
@@ -0,0 +1,3 @@
+import os
+os.mkdir("bharath")
+os.rename("bharath", "bharath-new")

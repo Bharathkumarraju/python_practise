ea2c31f5ab86d206438c62b8217549e2f02248e9
Bharath <bharath@hooq.tv>
2021-06-26 15:19:31 +0800

    Python for life

---
+++ Day20_June26_2021/command_output.sh
@@ -0,0 +1,30 @@
+>>> print("hello future python programmer!");\
+... print("python is a great language"); \
+... print("Strings don't get executed as code")
+hello future python programmer!
+python is a great language
+Strings don't get executed as code
+>>>
+
+
+>>> print("hello, \nfuture Python programmer!!!")
+hello,
+future Python programmer!!!
+>>> print("hello, \nfuture Python \nprogrammer!!!")
+hello,
+future Python
+programmer!!!
+>>> print("hello, \nfuture Python \n programmer!!!")
+hello,
+future Python
+ programmer!!!
+>>> print("hello, \nfuture Python \nprogrammer!!!")
+hello,
+future Python
+programmer!!!
+>>>
+
+
+
+
+
--- Day20_June26_2021/print_function.py
+++ Day20_June26_2021/print_function.py
@@ -1,5 +1,13 @@
-print("Hello future python progrmmer!")
-
+print("Hello future python programmer!")
 print("Python is a great language!!!")
-
 print("Strings don't get executed as a code")
+
+print("")
+
+print("Hello \nfuture \npython programmer!!!")
+print("hello", "future", "python", "programmer")
+
+print("")
+
+print("Hello! ", end="")
+print("Python is a great language!!!")

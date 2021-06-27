fc272c82980c61d4eaf0eb69a3dc233bbacaa46a
Bharath <bharath@hooq.tv>
2021-06-27 08:37:50 +0800

    All about python literals

---
+++ Day20_June26_2021/literals/demo.sh
@@ -0,0 +1,38 @@
+# In Python we can use literals in order to encode data and put them in the code.
+
+Literanl types
+
+1. Integers
+    i. Octal numbers    ---> 0o123
+    ii. Hexadecimal numbers
+2. Floating point numbers
+3. Strings
+4. Booleans
+
+Octal numbers  ---> 0o123
+===================================>
+2  1  0
+1  2  3
+
+8**2 * 1 + 8**1 * 2 + 8**0 * 3
+
+64 * 1 + 8 * 2 + 1 * 3
+
+64 + 16 + 3 = 83
+
+
+Hexadecimal numbers  ---> 0x123
+=======================================>
+
+2  1  0
+1  2  3
+
+
+16**2 * 1 + 16**1 * 2 + 16**0 * 3
+
+256 * 1  + 16 * 2  + 1 * 3
+
+256 + 32 + 3  = 291
+
+
+

371b72fec009b215ac7c49d2daf92e0c27cb8a92
Bharath <bharath@hooq.tv>
2021-06-27 09:20:53 +0800

    All about operators

---
+++ Day21_June27_2021/Operators/arithmetic_divide.py
@@ -0,0 +1,27 @@
+print(10 / 2)
+
+print(10. / 2)
+
+print(10 / 2.)
+
+print(10. / 2.)
+
+
+print(10 // 2)
+
+print(10. // 2)
+
+print(10 // 2.)
+
+print(10. // 2.)
+
+
+# floor divide rounds the value lesser integer value
+
+print(6. / 4)
+
+print(6. // 4)
+
+print(6. // -4)
+
+print(6. // -4)

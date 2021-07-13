57bdfc3b97208347939fc3fbf5faf19e37653db6
bharath <bharathkumar.dasaraju@singtel.com>
2021-07-13 08:43:40 +0800

    all about strings and input functions

--- python_certification_entrylevel/strings/example1.py
+++ python_certification_entrylevel/strings/example1.py
@@ -1,1 +1,10 @@
-erg
+print("ha" * 10)
+
+print("ha" * 0)
+print("ha" * -1)
+
+cost_of_apple = 2
+amount_of_apples = input("How many apples do you want? ")
+
+total_sum = cost_of_apple * int(amount_of_apples)
+print("You have to pay: " + str(total_sum))
---
+++ python_certification_entrylevel/strings/example2.py
@@ -0,0 +1,6 @@
+x = 5
+y = "Sally"
+print(str(x) + y)
+
+Num = input("Enter a Number: ")
+print(Num * 3)

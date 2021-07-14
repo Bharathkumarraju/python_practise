8e09c487230aa49d6d5d393e5377ef5ca8439eb0
bharath <bharathkumar.dasaraju@singtel.com>
2021-07-14 21:22:24 +0800

    All about python packages

---
+++ python_certification_entrylevel/python_modules/main.py
@@ -0,0 +1,17 @@
+import math as m
+from math import sqrt, pi, sin
+from math import *
+number = 25
+res = m.sqrt(number)
+print(res)
+print(m.pi)
+
+num = sqrt(144)
+print(num)
+
+value = sin(pi/2)
+print(value)
+
+
+import  math
+print(dir(math))

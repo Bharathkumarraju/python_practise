0a8e6ed0081a6dbd8f859b1b15ed2c77452829af
bharath <bharathkumar.dasaraju@singtel.com>
2021-07-16 14:31:07 +0800

    use urllib

---
+++ Manage_larger_programs/sample.py
@@ -0,0 +1,5 @@
+import sys
+print(sys.version_info[0])
+print(sys.version_info[1])
+print(sys)
+assert sys.version_info[0] == 3
---
+++ Manage_larger_programs/urllib_module.py
@@ -0,0 +1,11 @@
+import urllib
+import urllib.request
+
+from urllib import request
+
+print(type(urllib))
+print(type(urllib.request))
+
+# Packages are generally directories
+# Modules are generally files
+

f7ddad79deb48cd8a94aa45bc3674d512317a808
bharath <bharathkumar.dasaraju@singtel.com>
2021-07-28 06:37:48 +0800

    All about python simple loops

---
+++ Programiz_python_learning/while_loop.py
@@ -0,0 +1,6 @@
+count = 5
+
+while count <= 10:
+    print("This is a while loop ")
+    print(count)
+    count = count + 1
---
+++ Programiz_python_learning/while_multiplication.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a number!!!"))
+
+count = 1
+
+while count <= 10:
+    product = num * count
+    print(num, "x", count, "=", product)
+    count = count + 1
